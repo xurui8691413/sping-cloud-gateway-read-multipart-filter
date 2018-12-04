6c352ab36bcfc22eab068b87570f1a5f16ba5c19
xurui8691413 <xurui8611@126.com>
2018-12-04 18:45:02 +0800

    Update ReadMultipartBodyFilter.java

--- spring-cloud-gateway-sample/src/main/java/org/springframework/cloud/gateway/sample/ReadMultipartBodyFilter.java
+++ spring-cloud-gateway-sample/src/main/java/org/springframework/cloud/gateway/sample/ReadMultipartBodyFilter.java
@@ -79,17 +79,6 @@ public class ReadMultipartBodyFilter implements GlobalFilter, Ordered {
 
 	}
 	
-	/**
-	 *
-	 * @param data1
-	 * @param data2
-	 */
-	private byte[] addBytes(byte[] data1, byte[] data2) {
-		byte[] data3 = new byte[data1.length + data2.length];
-		System.arraycopy(data1, 0, data3, 0, data1.length);
-		System.arraycopy(data2, 0, data3, data1.length, data2.length);
-		return data3;
-	}
 
 	@Override
 	public int getOrder() {
